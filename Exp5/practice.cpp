a4d2f274ee9fd21db795697b3be796c86e353d22
Deepak-Ragavendra <deeppanbhu05@gmail.com>
2025-09-08 22:31:07 +0530

    Practice

--- Exp5/practice.cpp
+++ Exp5/practice.cpp
@@ -18,12 +18,30 @@ int cutrodmemo(int n,int p[],unordered_map<int,int> dp){
     if(dp.count(n)) return dp[n];
     int m=0;
     for(int i=1;i<=n;i++){
-        m=max(m,p[n-i]+cutrodmemo(n-i,p,dp));
+        m=max(m,p[n-1]+cutrodmemo(n-i,p,dp));
     }
     return dp[n]=m;
 }
 
-int cutrodtable(int n,int p[],vector<int> dp){
+int cutrodtable(int n,int p[],vector<int> prices){
     if(n==0) return 0;
+    vector<int> dp(c+1,0);
+    vector<int> cut(c+1,0);
+    for(int i=1;i<=c;i++){
+        int m=INT_MIN;
+        for(int j=1;j<=i;j++){
+            if(prices[j-1]+dp[i-j]>m){
+                m=prices[j-1]+dp[i-j];
+                cut[i]=j;
+            }
+        }
+        dp[i]=m;
+    }
+    int t=c;
+    while(t>0){
+        cout<<cut[t]<<endl;
+        t-=cut[t];
+    }
+    return dp[c];
     
 }
